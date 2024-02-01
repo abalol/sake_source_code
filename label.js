7977256c9bd63b218eebdb9d4f55f20ffff19d4d
Tomohiro Owada <info@tomoporo.net>
2024-02-01 20:52:06 +0800

    Update label.js

--- label.js
+++ label.js
@@ -4,30 +4,30 @@ console.log(
     [!![] + !![] + !![] + !![] + !![] + !![], !![] + !![] + !![] + !![] + !![] + !![] + !![]].join(
       ''
     )
-  ), // C
-  String.fromCodePoint([+!![], +!![], +!![]].join('')), // o
-  String.fromCodePoint([+!![], +!![], ~~[]].join('')), // n
-  String.fromCodePoint([+!![], ~~[], !![] + !![] + !![]].join('')), // g
-  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![]].join('')), // r
+  ),
+  String.fromCodePoint([+!![], +!![], +!![]].join('')),
+  String.fromCodePoint([+!![], +!![], ~~[]].join('')),
+  String.fromCodePoint([+!![], ~~[], !![] + !![] + !![]].join('')),
+  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![]].join('')),
   String.fromCodePoint(
     [
       !![] + !![] + !![] + !![] + !![] + !![] + !![] + !![] + !![],
       !![] + !![] + !![] + !![] + !![] + !![] + !![],
     ].join('')
-  ), // a
-  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![] + !![] + !![]].join('')), // t
-  String.fromCodePoint([+!![], +!![], +!![] + !![] + !![] + !![] + !![] + !![] + !![]].join('')), // u
-  String.fromCodePoint([+!![], ~~!![], !![] + !![] + !![] + !![]].join('')), // r
+  ),
+  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![] + !![] + !![]].join('')),
+  String.fromCodePoint([+!![], +!![], +!![] + !![] + !![] + !![] + !![] + !![] + !![]].join('')),
+  String.fromCodePoint([+!![], ~~!![], !![] + !![] + !![] + !![]].join('')),
   String.fromCodePoint(
     [
       !![] + !![] + !![] + !![] + !![] + !![] + !![] + !![] + !![],
       !![] + !![] + !![] + !![] + !![] + !![] + !![],
     ].join('')
-  ), // a
-  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![] + !![] + !![]].join('')), // t
-  String.fromCodePoint([+!![], ~~[], !![] + !![] + !![] + !![] + !![]].join('')), // i
-  String.fromCodePoint([+!![], +!![], +!![]].join('')), // o
-  String.fromCodePoint([+!![], +!![], ~~[]].join('')), // n
-  String.fromCodePoint([+!![], +!![], +!![] + !![] + !![] + !![] + !![]].join('')), // s
-  String.fromCodePoint([!![] + !![] + !![], !![] + !![] + !![]].join('')) // !
+  ),
+  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![] + !![] + !![]].join('')),
+  String.fromCodePoint([+!![], ~~[], !![] + !![] + !![] + !![] + !![]].join('')),
+  String.fromCodePoint([+!![], +!![], +!![]].join('')),
+  String.fromCodePoint([+!![], +!![], ~~[]].join('')),
+  String.fromCodePoint([+!![], +!![], !![] + !![] + !![] + !![] + !![]].join('')),
+  String.fromCodePoint([!![] + !![] + !![], !![] + !![] + !![]].join(''))
 );
